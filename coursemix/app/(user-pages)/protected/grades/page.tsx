8c17bb6e0b0e4a072ae60cf2d242c5ce830e4178
Russell Salacup <ta20eo@brocku.ca>
2025-02-26 15:26:11 -0500

    User must be logged in to access the Grades page.

--- coursemix/app/(user-pages)/protected/grades/page.tsx
+++ coursemix/app/(user-pages)/protected/grades/page.tsx
@@ -1,4 +1,21 @@
+// Author: Russell
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
+
 export default async function Grades () {
+  // This line has been borrowed from other pages "^w^
+  const supabase = await createClient();
+  
+  // User must be signed in to proceed
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+  
+  if (!user) {
+    // Means user isn't signed in
+    return redirect("/sign-in");
+  }
+
   return (
     <div>
       Under construction!
